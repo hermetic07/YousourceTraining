fbdb33775d9108ca53e729976a409b1bb89d7a72
Evander Macandog <evndr.ls@gmail.com>
2020-07-02 14:30:03 +0800

    Added handlers and classes

---
+++ Chain of Responsibility/Chain of Responsibility/Handlers/JsonRequestHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chain_of_Responsibility.Classes;
+
+namespace Chain_of_Responsibility.Handlers
+{
+    public class JsonRequestHandler : RequestParser
+    {
+        public override void Handle(Request _request)
+        {
+            if (_request.ContentType.ToUpper() == "JSON")
+            {
+                Console.WriteLine("{0} handled request",this.GetType().Name);
+            }
+            else if (successor != null)
+            {
+                successor.Handle(_request);
+            }
+        }
+    }
+}
--- Chain of Responsibility/Chain of Responsibility/Program.cs
+++ Chain of Responsibility/Chain of Responsibility/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Chain_of_Responsibility.Classes;
+using Chain_of_Responsibility.Handlers;
 
 namespace Chain_of_Responsibility
 {
@@ -10,6 +12,23 @@ namespace Chain_of_Responsibility
     {
         static void Main(string[] args)
         {
+            RequestParser _xml = new XmlRequestHandler();
+            RequestParser _json = new JsonRequestHandler();
+            RequestParser _base = new BaseRequestHandler();
+
+            _xml.setSuccessor(_json);
+            _json.setSuccessor(_base);
+
+            Request _request = new Request();
+
+            Console.Write("Content Type: ");
+            _request.ContentType = Console.ReadLine();
+            Console.Write("Content: ");
+            _request.Content = Console.ReadLine();
+
+            _xml.Handle(_request);
+
+            Console.ReadKey();
         }
     }
 }
