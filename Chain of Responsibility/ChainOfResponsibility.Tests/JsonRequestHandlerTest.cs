1b81873ea732820d15840c32b15119e1313f649a
Evander Macandog <evndr.ls@gmail.com>
2020-07-07 17:04:28 +0800

    Chain of responsibility Unit test

---
+++ Chain of Responsibility/ChainOfResponsibility.Tests/JsonRequestHandlerTest.cs
@@ -0,0 +1,39 @@
+﻿namespace ChainOfResponsibility.Tests
+{
+    using Chain_of_Responsibility.Classes;
+    using Chain_of_Responsibility.Handlers;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    [TestClass]
+    public class JsonRequestHandlerTest
+    {
+        Mock<JsonRequestHandler> target;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.target = new Mock<JsonRequestHandler>();
+        }
+
+        [TestCleanup]
+        public void Teardown()
+        {
+            this.target = null;
+        }
+
+        [TestMethod]
+        public void Parse_ValidJsonRequest_ReturnsVoid()
+        {
+            Request request = new Request()
+            {
+                ContentType = "json",
+                Content = @"{""name"":""John Doe"",""age"":20}"
+            };
+
+            this.target.Object.Handle(request);
+
+            this.target.Verify(a => a.Handle(It.IsAny<Request>()), Times.Once);
+        }
+    }
+}
---
+++ Chain of Responsibility/ChainOfResponsibility.Tests/XMLRequestHandlerTest.cs
@@ -0,0 +1,46 @@
+﻿namespace ChainOfResponsibility.Tests
+{
+    using Chain_of_Responsibility.Classes;
+    using Chain_of_Responsibility.Handlers;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    [TestClass]
+    public class XMLRequestHandlerTest
+    {
+        Mock<XmlRequestHandler> target;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.target = new Mock<XmlRequestHandler>();
+        }
+
+        [TestCleanup]
+        public void Teardown()
+        {
+            this.target = null;
+        }
+
+        [TestMethod]
+        public void Parse_ValidXmlRequest_ReturnsVoid()
+        {
+            Request request = new Request()
+            {
+                ContentType = "xml",
+                Content = @"<PurchaseOrder>
+                                <PurchasedGame>
+                                    <Name>GTA</Name>
+                                </PurchasedGame>
+                                <Customer>
+                                    <Name>Evander</Name>
+                                </Customer>
+                            </PurchaseOrder>"
+            };
+
+            this.target.Object.Handle(request);
+
+            this.target.Verify(a => a.Handle(It.IsAny<Request>()), Times.Once);
+        }
+    }
+}
